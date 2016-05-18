d1333591511024db3def9298e048d8e7497c677c
AlfredYang1986 <yangyuanpig@gmail.com>
2016-05-18 21:13:46 +0800

    第二个页面

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,11 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+    def index = Action {
+        Ok(views.html.index("Your new application is ready."))
+    }
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
+    def certificate = Action {
+        Ok(views.html.certificate_index("test"))
+    }
 }
