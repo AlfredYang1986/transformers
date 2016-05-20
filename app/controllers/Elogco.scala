02e295b59c78d38ef54a87f99f33b01d5963951c
AlfredYang1986 <yangyuanpig@gmail.com>
2016-05-20 14:17:57 +0800

    五个首页面

---
+++ app/controllers/Elogco.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Elogco extends Controller {
+  
+    /**
+     * home 
+     */
+    def index = Action {
+        Ok(views.html.index("Your new application is ready."))
+    }
+    
+    /**
+     * company
+     */
+    def company = Action {
+        Ok(views.html.company("Your new application is ready."))
+    }
+    
+    /**
+     * business
+     */
+    def business = Action {
+        Ok(views.html.business("Your new application is ready."))
+    }
+    
+    /**
+     * recruit
+     */
+    def recruit = Action {
+        Ok(views.html.recruit("Your new application is ready."))
+    }
+    
+    /**
+     * help
+     */
+    def help = Action {
+        Ok(views.html.help("Your new application is ready."))
+    }
+
+    def certificate = Action {
+        Ok(views.html.certificate_index("test"))
+    }
+}
