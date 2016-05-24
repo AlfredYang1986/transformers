5ff96585e0a9d111451d753418dccd3dfe33cd0f
AlfredYang1986 <yangyuanpig@gmail.com>
2016-05-24 20:07:58 +0800

    admin pages

---
+++ app/controllers/AdminController.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object AdminController extends Controller {
+    def index = Action {
+        Ok(views.html.admin_index())
+    }
+    
+    def passCompanyPage = Action {
+        Ok(views.html.admin_company_pass())
+    }
+    
+    def passIndustryPage = Action {
+        Ok(views.html.admin_industry_pass())
+    }
+    
+    def passSpicalwayPage = Action {
+        Ok(views.html.admin_spicalway_pass())
+    }
+    
+    def passDriverPage = Action {
+        Ok(views.html.admin_driver_pass())
+    }
+    
+    def faildCompanyPage = Action {
+        Ok(views.html.admin_company_faild())
+    }
+    
+    def faildIndustryPage = Action {
+        Ok(views.html.admin_industry_faild())
+    }
+    
+    def faildSpicalwayPage = Action {
+        Ok(views.html.admin_spicalway_faild())
+    }
+    
+    def faildDriverPage = Action {
+        Ok(views.html.admin_driver_faild())
+    }
+}
