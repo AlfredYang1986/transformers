c2ec837c9fbaba1e456379f371487a4237388a99
Lynn Shen <bjywan@hotmail.com>
2016-08-08 13:18:05 +1000

    company industry pages merge done and bug for sidebar
    sidebar doesn't work in the company/industry page as well.

---
+++ app/controllers/CompanyIndustryController.scala
@@ -0,0 +1,136 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object CompanyIndustryController extends Controller {
+  
+    /**
+     * Company Login Page
+     */
+    def ciLoginIndex = Action {
+        Ok(views.html.ciLoginIndex("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginAccountExtra = Action {
+        Ok(views.html.ciLoginAccountExtra("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginAccountNormalInfo = Action {
+        Ok(views.html.ciLoginAccountNormalInfo("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginAccountPeople = Action {
+        Ok(views.html.ciLoginAccountPeople("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginAccountProductName = Action {
+        Ok(views.html.ciLoginAccountProductName("Your new application is ready."))
+    }
+
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginAccountPsw = Action {
+        Ok(views.html.ciLoginAccountPsw("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginAccountValidateInfo = Action {
+        Ok(views.html.ciLoginAccountValidateInfo("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginCompanyList = Action {
+        Ok(views.html.ciLoginCompanyList("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginCompleteInfo = Action {
+        Ok(views.html.ciLoginCompleteInfo("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginCompleteProduct = Action {
+        Ok(views.html.ciLoginCompleteProduct("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginDriverList = Action {
+        Ok(views.html.ciLoginDriverList("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginRecruitment = Action {
+        Ok(views.html.ciLoginRecruitment("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginSendInfo = Action {
+        Ok(views.html.ciLoginSendInfo("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginSendProduct = Action {
+        Ok(views.html.ciLoginSendProduct("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginSentInfo = Action {
+        Ok(views.html.ciLoginSentInfo("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginSentProduct = Action {
+        Ok(views.html.ciLoginSentProduct("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginSpecialWayList = Action {
+        Ok(views.html.ciLoginSpecialWayList("Your new application is ready."))
+    }
+
+    /**
+     * Company Login Page
+     */
+    def ciLoginRewards = Action {
+        Ok(views.html.ciLoginRewards("Your new application is ready."))
+    }
+
+
+}
