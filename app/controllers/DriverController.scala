19582696f6338663d987cfc0eaa164f4e1d68eb1
Lynn Shen <bjywan@hotmail.com>
2016-08-08 10:48:45 +1000

    driver merge done & bug with side bar
    all the driver pages are merged into the current structure. however, the
    side bar are not working with id="close-sidebar" that causes the mobile
    view not responsive.

--- app/controllers/DriverController.scala
+++ app/controllers/DriverController.scala
@@ -19,6 +19,54 @@ object DriverController extends Controller {
     	Ok(views.html.driverLoginAccountPsw("Your new application is ready."))
     }
 
+    /**
+     * Driver Account Normal Information (the driver could modify)
+     */
+    def driverLoginAccountNormalInfo = Action {
+        Ok(views.html.driverLoginAccountNormalInfo("Your new application is ready."))
+    }
 
-    
+    /**
+     * Driver Account Validate Information
+     */
+    def driverLoginAccountValidateInfo = Action {
+        Ok(views.html.driverLoginAccountValidateInfo("Your new application is ready."))
+    }
+
+    /**
+     * Driver Recruitment
+     */
+    def driverLoginRecruitment = Action {
+        Ok(views.html.driverLoginRecruitment("Your new application is ready."))
+    }
+
+    /**
+     * Driver Followed Company
+     */
+    def driverLoginAccountFollowedCompany = Action {
+        Ok(views.html.driverLoginAccountFollowedCompany("Your new application is ready."))
+    }
+
+    /**
+     * Driver Search Company
+     */
+    def driverLoginSearchCompany = Action {
+        Ok(views.html.driverLoginSearchCompany("Your new application is ready."))
+    }
+
+    /**
+     * Driver Search Department
+     */
+    def driverLoginSearchDepartment = Action {
+        Ok(views.html.driverLoginSearchDepartment("Your new application is ready."))
+    }
+
+
+    /**
+     * Driver Search Special Way
+     */
+    def driverLoginSearchSpecialWay = Action {
+        Ok(views.html.driverLoginSearchSpecialWay("Your new application is ready."))
+    }
+
 }
