dfa804a3c275adf1ef51d22f41bc92bab84b2f2c
AlfredYang1986 <yangyuanpig@gmail.com>
2016-10-21 14:53:14 +0800

    省市区不兼容浏览器问题

--- public/javascripts/district-change2.js
+++ public/javascripts/district-change2.js
@@ -1,28 +1,30 @@
 function cityChanges(ele, container, index) {
 	var a = Number($(ele).attr("data-index"));
 	var province = $(ele).val();
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children().css({"display":"none"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="choice"]').css({"display":"block"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="' + province + '"]').css({"display":"block"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="' + province + '"]').first().attr("selected", "true")
+
+	var s = $(container).find('select[data-index="' + String(a + 1) + '"]');
+	s.empty();
+
+	$.each($('li[data-category="city"][data-filter="' + province + '"]'), function() {
+		var v = $(this).attr("data-value");
+		$("<option value='" + v  + "'>" + v + "</option>").appendTo(s);
+	});
 }
 
 function cityChanges2(ele, container, index) {
-	var a = Number($(ele).attr("data-index"));
-	var province = $(ele).val();
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children().css({"display":"none"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="choice"]').css({"display":"block"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="' + province + '"]').css({"display":"block"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="' + province + '"]').first().attr("selected", "true")
-
+	cityChanges(ele, container, index);
 	districtChanges($(container).find('select[data-index="' + String(a + 1) + '"]'), container, index);
 }
 
 function districtChanges(ele, container, index) {
 	var a = Number($(ele).attr("data-index"));
 	var city = $(ele).val();
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children().css({"display":"none"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="choice"]').css({"display":"block"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="' + city + '"]').css({"display":"block"});
-	$(container).find('select[data-index="' + String(a + 1) + '"]').children('option[data-filter="' + city + '"]').first().attr("selected", "true");
+
+	var s = $(container).find('select[data-index="' + String(a + 1) + '"]');
+	s.children().remove();
+
+	$.each($('li[data-category="district"][data-filter="' + city + '"]'), function() {
+		var v = $(this).attr("data-value");
+		$("<option value='" + v  + "'>" + v + "</option>").appendTo(s);
+	});
 }
