689d56024731494856b0217d594986348ddceb2a
AlfredYang1986 <yangyuanpig@gmail.com>
2016-10-21 19:51:28 +0800

    效能问题 猜测

--- app/module/common/xml/xmlOpt.scala
+++ app/module/common/xml/xmlOpt.scala
@@ -9,12 +9,18 @@ import play.api.libs.json.JsValue
 object xmlOpt {
     lazy val doc_cities = xml.XML.loadFile("resource/city_defines.xml")
     
-    val allCities : List[JsValue] =
-        ((doc_cities \ "province") map ( x => 
-            toJson(Map("province" -> toJson((x \ "@name").text),
-                       "cities" -> toJson((x \ "city") map (y => y \ "@name") map (y => y.text)),
-                       "city_ditails" -> toJson((x \ "city") map ( c => 
-                           Map("city" -> toJson((c \ "@name").text),
-                               "districts" -> toJson((c \ "district") map (d => (d \ "@name")) map (z => (z.text))))
-                       )))))).toList
+    var allCities_val : List[JsValue] = Nil
+                       
+    def allCities : List[JsValue] = 
+      if (allCities_val.isEmpty) {
+        allCities_val = 
+            ((doc_cities \ "province") map ( x => 
+                toJson(Map("province" -> toJson((x \ "@name").text),
+                           "cities" -> toJson((x \ "city") map (y => y \ "@name") map (y => y.text)),
+                           "city_ditails" -> toJson((x \ "city") map ( c => 
+                               Map("city" -> toJson((c \ "@name").text),
+                                   "districts" -> toJson((c \ "district") map (d => (d \ "@name")) map (z => (z.text))))
+                           )))))).toList
+        allCities_val
+      } else allCities_val
 }
