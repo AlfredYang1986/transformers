43bfab45c8795763e94b9cad848891d9d3fd5e44
AlfredYang1986 <yangyuanpig@gmail.com>
2016-10-25 09:10:25 +0800

    几个bug的修改

--- app/module/system/config/ConfigModule.scala
+++ app/module/system/config/ConfigModule.scala
@@ -14,7 +14,7 @@ object ConfigModule {
         val builder = MongoDBObject.newBuilder
         builder += "index" -> 1
         builder += "vehicle" -> ("平板车" :: "高栏车" :: "厢式车" :: "半挂车" :: "高低板" :: "起重车" :: "牵引车" :: "仓棚车" :: "罐式车" :: "特种车" :: "危险品车" :: "保温冷藏" :: "集装箱车" :: Nil)
-        builder += "vehicle_length" -> (4.2 :: 4.5 :: 5.0 :: 6.2 :: 6.9 :: 7.2 :: 7.7 :: 7.8 :: 8.2 :: 8.6 :: 8.7 :: 9.6 :: 11.7 :: 12.5 :: 13.0 :: 13.5 :: 14.0 :: 16.0 :: 17.0 :: 17.5 :: 18.0 :: Nil) 
+        builder += "vehicle_length" -> (4.2 :: 4.5 :: 5.2 :: 6.2 :: 6.8 :: 7.2 :: 7.7 :: 7.8 :: 8.2 :: 8.6 :: 8.7 :: 9.6 :: 11.7 :: 12.5 :: 13.0 :: 13.5 :: 14.0 :: 16.0 :: 17.0 :: 17.5 :: 18.0 :: Nil)
         _data_connection.getCollection("system_config") += builder.result
     }
     
